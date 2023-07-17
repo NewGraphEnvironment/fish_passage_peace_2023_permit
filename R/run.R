4f4e8814ab4459ba898ac1e2aba602b3de8ceba5
Allan Irvine <46538103+NewGraphEnvironment@users.noreply.github.com>
2023-07-17 14:08:58 -0700

    Initial commit

---
+++ R/run.R
@@ -0,0 +1,17 @@
+
+##to build both a paged html version and a gitbook follow the steps below
+
+#######################################################################################
+##change your VErsion #
+#######################################################################################
+
+
+
+##move the html to the docs folder so it can be viewed online
+file.rename('application_moe_revised.html', 'docs/index.html')
+file.rename('application_moe_revised.pdf', 'docs/application_moe.pdf')
+
+
+
+
+
